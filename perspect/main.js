b506154f47cb51def5edd29ffb9e5ca71e6f7197
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2015-01-23 11:05:59 +0100

    Improve event handler to do less computations and avoid flickering at region borders.

--- perspect/main.js
+++ perspect/main.js
@@ -1,3 +1,4 @@
+window.onload = function () {
 var threed = document.getElementById("threed");
 threedHoverHandler = setClassFromMousePos.bind(threed);
 threed.addEventListener("mousemove", threedHoverHandler);
@@ -7,11 +8,13 @@ function setClass(className) {
     this.className = className;
 }
 
+var rect = threed.parentNode.getBoundingClientRect()
+var pointLocator = mkPointLocator(rect);
+
 function setClassFromMousePos(e) {
-    var rect = this.getBoundingClientRect()
     var x = e.pageX - rect.left,
         y = e.pageY - rect.top;
-    var loc = locatePoint(rect, x, y);
+    var loc = pointLocator(x, y);
     setClass.call(this, 'tilt-' + loc);
 }
 
@@ -32,3 +35,15 @@ function locatePoint(rect, x, y) {
     }
     return row*3 + col;
 }
+
+function mkPointLocator(rect) {
+    var order = 3;
+    var dw = rect.width / order,
+        dh = rect.height / order;
+
+    return function (x, y) {
+        var col = Math.floor(x / dw),
+            row = Math.floor(y / dh);
+        return row * order + col;
+    }
+}}
