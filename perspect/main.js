8b48030fabfe4f0d0e69e7971f040a1d3216ea71
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2015-01-23 14:57:15 +0100

    Add transition for a slick effect.

--- perspect/main.js
+++ perspect/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
 var threed = document.getElementById("threed");
 threedHoverHandler = setClassFromMousePos.bind(threed);
 threed.addEventListener("mousemove", threedHoverHandler);
-threed.addEventListener("mouseout", setClass.bind(threed, ''));
+threed.addEventListener("mouseout", setClass.bind(threed, 'tilt'));
 
 function setClass(className) {
     this.className = className;
@@ -15,25 +15,7 @@ function setClassFromMousePos(e) {
     var x = e.pageX - rect.left,
         y = e.pageY - rect.top;
     var loc = pointLocator(x, y);
-    setClass.call(this, 'tilt-' + loc);
-}
-
-function locatePoint(rect, x, y) {
-    var col = 1;
-    var dw = rect.width / 3;
-    if (x < dw) {
-        col = 0;
-    } else if (x > 2*dw) {
-        col = 2;
-    }
-    var row = 1;
-    var dh = rect.height / 3;
-    if (y < dh) {
-        row = 0;
-    } else if (y > 2*dh) {
-        row = 2;
-    }
-    return row*3 + col;
+    setClass.call(this, 'tilt tilt-' + loc);
 }
 
 function mkPointLocator(rect) {
